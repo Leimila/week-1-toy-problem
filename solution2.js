6eef193e4dc283ac20c85bb4770b609664b7202e
Leimila <leimilamitei2016@gmail.com>
2024-10-27 23:02:22 +0300

    Speed Detecter

--- solution2.js
+++ solution2.js
@@ -1,13 +1,17 @@
 function checkSpeed(speed) {
     const speedLimit = 70;
     const pointsPer5k =1;
+    // If the speed is within the speed limit or less, print "OK"
     if (speed <= speedLimit) {
         console.log("OK");
         } else {
+            // Calculate the number of points by finding how many multiples of 5 km/h over the speed limit the driver is going
             const points = Math.floor((speed -speedLimit) / 5) ;
+            // If the points exceed 12, print "License suspended"
             if (points >12){
                 console.log("License suspended");
                 } else {
+                    // Otherwise, print the number of points
                     console.log(`points: ${points}`);
 
                 
